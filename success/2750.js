ae0a860cd8c50b1f59f8e4617432c16c7db49098
seunghwan Choi <seung1625@gmail.com>
2022-03-21 15:48:53 +0900

    2750_수정

--- success/2750.js
+++ success/2750.js
@@ -3,8 +3,8 @@ let n = Number(input[0]);
 
 let items = [];
 for (let i = 0; i < n; i++) {
-	items[i] = Number(input[i+1]);
+	items[i] = Number(input[i + 1]);
 }
 
 items.sort((a, b) => a - b);
-items.forEach(item => console.log(item));
+items.forEach((item) => console.log(item));
